1c772e1a00e430e64008fa215ac6bfea5963a98d
Felix Resch <felix.resch@tuwien.ac.at>
2021-11-11 18:00:32 +0100

    made syscalls wrappers unsafe as they might abort when called wrongly
    added high level gpio pin wrapper
    changed rusty typed syscall wrappers to be unsafe
    changed constructors of high level wrappers to be unsafe
    added documentation

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,33 +1,90 @@
+//! Syscalls and high level wrappers for the Zephyr GPIO API.
+//!
+//! Some functions in the high level API still are marked as `unsafe` as the required checks cannot be
+//! performed to offer a safe API.
 
+use crate::{Context, ZephyrError};
 pub use zephyr_sys::raw::device as Device;
-pub use zephyr_sys::raw::{gpio_pin_t as GpioPin, gpio_flags_t as GpioFlags};
+pub use zephyr_sys::raw::{gpio_flags_t as GpioFlags, gpio_pin_t as GpioPinNumber};
 
-pub fn pin_configure(port: &Device, pin: GpioPin, flags: GpioFlags) -> Result<(), ()> {
-    let ret = unsafe {
-        zephyr_sys::syscalls::any::gpio_pin_configure(port as *const Device, pin, flags)
-    };
+const CONTEXT: GpioWrapperContext = GpioWrapperContext {};
 
-    if ret == 0 {
+/// Safe wrapper for the `gpio_pin_configure` syscall.
+///
+/// `device` MUST be a `'static` reference to a device descriptor obtained from the Zephyr API.
+/// This wrapper calls the syscall and wraps the error number in a safe error type.
+///
+/// This method might fail/panic/abort if the device is not a gpio device.
+pub unsafe fn pin_configure(
+    port: &Device,
+    pin: GpioPinNumber,
+    flags: GpioFlags,
+) -> Result<(), ZephyrError> {
+    let errno = zephyr_sys::syscalls::any::gpio_pin_configure(port as *const Device, pin, flags);
+
+    if errno == 0 {
         Ok(())
     } else {
-        Err(())
+        Err(ZephyrError::from_errno_with_context(errno, &CONTEXT))
     }
 }
 
-pub fn pin_set_raw(port: &Device, pin: GpioPin, value: bool) -> Result<(), ()> {
-    let ret = if value {
-        unsafe {
-            zephyr_sys::syscalls::any::gpio_port_set_bits_raw(port as *const Device, 1 << pin)
-        }
+/// Safe wrapper for the `gpio_pin_set_raw` syscall.
+///
+/// `device` MUST be a `'static` reference to a device descriptor obtained from the Zephyr API.
+/// This wrapper calls the syscall and wraps the error number in a safe error type.
+///
+/// This method might fail/panic/abort if the device is not a gpio device.
+pub unsafe fn pin_set_raw(
+    port: &Device,
+    pin: GpioPinNumber,
+    value: bool,
+) -> Result<(), ZephyrError> {
+    let errno = if value {
+        zephyr_sys::syscalls::any::gpio_port_set_bits_raw(port as *const Device, 1 << pin)
     } else {
-        unsafe {
-            zephyr_sys::syscalls::any::gpio_port_clear_bits_raw(port as *const Device, 1 << pin)
-        }
+        zephyr_sys::syscalls::any::gpio_port_clear_bits_raw(port as *const Device, 1 << pin)
     };
 
-    if ret == 0 {
+    if errno == 0 {
         Ok(())
     } else {
-        Err(())
+        Err(ZephyrError::from_errno_with_context(errno, &CONTEXT))
+    }
+}
+
+#[derive(Debug)]
+struct GpioWrapperContext {}
+
+impl Context for GpioWrapperContext {
+    fn name(&self) -> &'static str {
+        "gpio wrapper context"
+    }
+}
+
+/// High level wrapper for a GPIO pin.
+pub struct GpioPin {
+    device: &'static Device,
+    pin_number: GpioPinNumber,
+}
+
+impl GpioPin {
+    /// Creates a new [GpioPin] on the current interface.
+    ///
+    /// `device` MUST be a gpio device. If `device` is not a gpio device the behaviour
+    /// when calling any method is undefined!
+    pub unsafe fn new(
+        device: &'static Device,
+        pin_number: GpioPinNumber,
+        flags: GpioFlags,
+    ) -> Result<Self, ZephyrError> {
+        pin_configure(device, pin_number, flags)?;
+        Ok(GpioPin { device, pin_number })
+    }
+
+    /// Set the state of the GPIO pin.
+    pub fn set_value(&mut self, value: bool) -> Result<(), ZephyrError> {
+        // device MUST BE a gpio device as per the constructor
+        unsafe { pin_set_raw(self.device, self.pin_number, value) }
     }
 }
--- src/sensor.rs
+++ src/sensor.rs
@@ -1,9 +1,55 @@
+//! Syscalls and high level wrappers for the Zephyr Sensor API.
+//!
+//! Some functions in the high level API still are marked as `unsafe` as the required checks cannot be
+//! performed to offer a safe API.
+//!
+//! ```should_panic
+//!# use zephyr::context::Kernel as Context;
+//! use zephyr_rust_wrappers::sensor::{Sensor, SensorChannel};
+//!
+//! let mut sensor = if let Some(sensor_device) = Context::device_get_binding("binding-label") {
+//!     // we must make sure manually that we are binding to a sensor device
+//!     unsafe {
+//!         Sensor::new(sensor_device)
+//!     }
+//! } else {
+//!     panic!("could not resolve binding for sensor")
+//! };
+//!
+//! let value = sensor
+//!                   .sample(SensorChannel::AmbientTemperature)
+//!                   .expect("sampling value from sensor");
+//!
+//! println!("sensor measures {} °C", value as f32)
+//! ```
 
 pub use zephyr::device::Device;
 
-use zephyr_sys::raw::sensor_value as ZSensorValue;
+use crate::{Context, ErrorNumber, ZephyrError};
 pub use zephyr_sys::raw::sensor_trigger as SensorTrigger;
+use zephyr_sys::raw::sensor_value as ZSensorValue;
 
+const CONTEXT: SensorWrapperContext = SensorWrapperContext {};
+
+/// Type that uses the internal Zephyr representation of a sensor value.
+/// This type is needed to add additional functionality to a sensor value (e.g. [Into<f32>]).
+///
+/// As per the Zephyr documentation, the value can be converted to a floating point using the formula
+/// <code>value = val1 + val2 * 10<sup>-6</sup></code>.
+///
+/// We define a [SensorValue] to be normalized if <code>0 <= val2 * 10<sup>-6</sup> < 1</code>. All
+/// conversions that are defined for our implementation will always yield a normalized sensor value.
+///
+/// This type implements conversions to and from [i32] and [f32].
+///
+/// ```rust
+///# use zephyr_rust_wrappers::sensor::SensorValue;
+/// let sensor_float: SensorValue = 1.5.into();
+/// let sensor_int: SensorValue = 1.into();
+///
+/// assert_eq!(1.5_f32, sensor_float.into());
+/// assert_eq!(1_u32, sensor_float.into());
+/// ```
 #[derive(Copy, Clone)]
 pub struct SensorValue {
     val1: i32,
@@ -37,128 +83,268 @@ impl From<i32> for SensorValue {
     }
 }
 
+impl From<SensorValue> for i32 {
+    fn from(other: SensorValue) -> Self {
+        other.val1
+    }
+}
+
 impl From<SensorValue> for f32 {
     fn from(other: SensorValue) -> Self {
         (other.val1 as f32) + (other.val2 as f32 * 1e-6_f32)
     }
 }
 
+impl From<f32> for SensorValue {
+    fn from(other: f32) -> Self {
+        let val1 = other.floor() as i32;
+        let val2 = ((other - val1 as f32) * (1e6_f32)) as i32;
+        Self { val1, val2 }
+    }
+}
+
+/// Non-exhaustive list of sensor channels. The list uses the values from Zephyr header files and
+/// might fail to compile if two or more sensor channels use the same representation.
 #[repr(u32)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub enum SensorChannel {
     AmbientTemperature = zephyr_sys::raw::sensor_channel_SENSOR_CHAN_AMBIENT_TEMP,
     Pressure = zephyr_sys::raw::sensor_channel_SENSOR_CHAN_PRESS,
 }
 
+/// Non-exhaustive list of sensor attributes. The list uses the values from Zephyr header files and
+/// might fail to compile if two or more sensor channels use the same representation.
 #[repr(u32)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub enum SensorAttribute {
     LowerThreshold = zephyr_sys::raw::sensor_attribute_SENSOR_ATTR_LOWER_THRESH,
     UpperThreshold = zephyr_sys::raw::sensor_attribute_SENSOR_ATTR_UPPER_THRESH,
 }
 
+/// Non-exhaustive list of sensor trigger types. The list uses the values from Zephyr header files and
+/// might fail to compile if two or more sensor channels use the same representation.
 #[repr(u32)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub enum TriggerType {
     Threshold = zephyr_sys::raw::sensor_trigger_type_SENSOR_TRIG_THRESHOLD,
     DataReady = zephyr_sys::raw::sensor_trigger_type_SENSOR_TRIG_DATA_READY,
 }
 
-pub fn sample_fetch_channel(device: &Device, sensor_channel: SensorChannel) -> Result<(), ()> {
-    let err = unsafe {
-        zephyr_sys::syscalls::any::sensor_sample_fetch_chan(device as *const Device, sensor_channel as u32)
+/// Wrapper to the `sensor_sample_fetch_chan` syscall.
+///
+/// `device` MUST be a `'static` reference to a device descriptor obtained from the Zephyr API.
+/// This wrapper calls the syscall and wraps the error number in a safe error type.
+///
+/// This method will fail if the sensor does not support `sensor_channel`.
+pub unsafe fn sample_fetch_channel(
+    device: &Device,
+    sensor_channel: SensorChannel,
+) -> Result<(), ZephyrError> {
+    let errno = unsafe {
+        zephyr_sys::syscalls::any::sensor_sample_fetch_chan(
+            device as *const Device,
+            sensor_channel as u32,
+        )
     };
 
-    if err == 0 {
+    if errno == 0 {
         Ok(())
     } else {
-        Err(())
+        Err(ZephyrError::from_errno_with_context(errno, &CONTEXT))
     }
 }
 
-pub fn channel_get(device: &Device, sensor_channel: SensorChannel) -> Result<SensorValue, ()> {
-    let mut z_sensor_value = ZSensorValue { val1: 0, val2: 0};
-    let err = unsafe {
-        zephyr_sys::syscalls::any::sensor_channel_get(device as *const Device, sensor_channel as u32, (&mut z_sensor_value) as *mut ZSensorValue)
+/// Wrapper to the `sensor_channel_get` syscall.
+///
+/// `device` MUST be a `'static` reference to a device descriptor obtained from the Zephyr API.
+/// This wrapper calls the syscall and wraps the error number in a safe error type.
+///
+/// This method will fail if the sensor does not support `sensor_channel`.
+pub unsafe fn channel_get(
+    device: &Device,
+    sensor_channel: SensorChannel,
+) -> Result<SensorValue, ZephyrError> {
+    let mut z_sensor_value = ZSensorValue { val1: 0, val2: 0 };
+    let errno = unsafe {
+        zephyr_sys::syscalls::any::sensor_channel_get(
+            device as *const Device,
+            sensor_channel as u32,
+            (&mut z_sensor_value) as *mut ZSensorValue,
+        )
     };
 
-    if err == 0 {
+    if errno == 0 {
         Ok(z_sensor_value.into())
     } else {
-        Err(())
+        Err(ZephyrError::from_errno_with_context(errno, &CONTEXT))
     }
 }
 
-pub fn attr_set(device: &Device, sensor_channel: SensorChannel, sensor_attribute: SensorAttribute, value: SensorValue) -> Result<(), ()> {
+/// Wrapper to the `sensor_channel_get` syscall.
+///
+/// `device` MUST be a `'static` reference to a device descriptor obtained from the Zephyr API.
+/// This wrapper calls the syscall and wraps the error number in a safe error type.
+///
+/// This method might fail if the sensor driver does not expose this driver call or if the sensor
+/// does not support the channel or the channel does not support the attribute.
+pub unsafe fn attr_set(
+    device: &Device,
+    sensor_channel: SensorChannel,
+    sensor_attribute: SensorAttribute,
+    value: SensorValue,
+) -> Result<(), ZephyrError> {
     let z_sensor_value: ZSensorValue = value.into();
-    let err = unsafe {
-        zephyr_sys::syscalls::any::sensor_attr_set(device as *const Device, sensor_channel as u32, sensor_attribute as u32, (&z_sensor_value) as *const ZSensorValue)
+    let errno = unsafe {
+        zephyr_sys::syscalls::any::sensor_attr_set(
+            device as *const Device,
+            sensor_channel as u32,
+            sensor_attribute as u32,
+            (&z_sensor_value) as *const ZSensorValue,
+        )
     };
 
-    if err == 0 {
+    if errno == 0 {
         Ok(())
     } else {
-        Err(())
+        Err(ZephyrError::from_errno_with_context(errno, &CONTEXT))
     }
 }
 
-pub fn trigger_set(device: &Device, sensor_trigger: &SensorTrigger, f: extern "C" fn(dev: &'static Device, trigger: &SensorTrigger)) -> Result<(), ()> {
+/// Implementation of the high level function `sensor_trigger_set` which is inlined in the Zephyr API.
+/// The method essentially does the same as the original inline function but also checks if the driver
+/// API is present in `device`.
+///
+/// `device` MUST be a `'static` reference to a device descriptor obtained from the Zephyr API.
+/// This wrapper calls the syscall and wraps the error number in a safe error type.
+///
+/// This method might fail if the sensor driver does not expose this driver call or if the sensor
+/// does not support the channel or the channel does not support the attribute.
+pub unsafe fn trigger_set(
+    device: &Device,
+    sensor_trigger: &SensorTrigger,
+    f: extern "C" fn(dev: &'static Device, trigger: &SensorTrigger),
+) -> Result<(), ZephyrError> {
     use zephyr_sys::raw::sensor_driver_api as SensorDriverApi;
-    let api: Option<&SensorDriverApi> = unsafe {
-        std::mem::transmute(device.api)
-    };
+    let api: Option<&SensorDriverApi> = std::mem::transmute(device.api);
 
     if let Some(api) = api {
         if let Some(trigger_set) = api.trigger_set {
-            let ret = unsafe {
-                let callback: extern "C" fn(dev: *const Device, trigger: *const SensorTrigger) = std::mem::transmute(f);
-                (trigger_set)(device as *const Device, sensor_trigger as *const SensorTrigger, Some(callback))
-            };
-            if ret == 0 {
+            let callback: extern "C" fn(dev: *const Device, trigger: *const SensorTrigger) =
+                std::mem::transmute(f);
+            let errno = (trigger_set)(
+                device as *const Device,
+                sensor_trigger as *const SensorTrigger,
+                Some(callback),
+            );
+
+            if errno == 0 {
                 Ok(())
             } else {
-                Err(())
+                Err(ZephyrError::from_errno_with_context(errno, &CONTEXT))
             }
         } else {
-            Err(())
+            Err(ZephyrError::new_with_context(
+                ErrorNumber::NNotImplemented,
+                &CONTEXT,
+            ))
         }
     } else {
-        Err(())
+        Err(ZephyrError::new_with_context(
+            ErrorNumber::NotImplemented,
+            &CONTEXT,
+        ))
     }
 }
 
+/// High level wrapper for a sensor.
+///
+/// This essentially wraps the static reference of the underlying device and offers methods that use
+/// the safe syscall wrappers for an object oriented programming interface.
 pub struct Sensor {
     device: &'static Device,
 }
 
 impl Sensor {
-    pub fn new(device: &'static Device) -> Self {
+    /// Creates a new [Sensor] on the current interface.
+    ///
+    /// `device` MUST be a sensor representing a device. If `device` is not a sensor the behaviour
+    /// when calling any method is undefined!
+    pub unsafe fn new(device: &'static Device) -> Self {
         Sensor { device }
     }
 
-    pub fn sample(&mut self, channel: SensorChannel) -> Result<SensorValue, ()> {
-        sample_fetch_channel(self.device, channel)?;
-        channel_get(self.device, channel)
+    /// Fetch `channel` and then read the value from the internal buffer.
+    ///
+    /// This method might fail if the sensor does not support the requested channel.
+    pub fn sample(&mut self, channel: SensorChannel) -> Result<SensorValue, ZephyrError> {
+        // device is required to be a sensor device in constructor
+        unsafe {
+            sample_fetch_channel(self.device, channel)?;
+            channel_get(self.device, channel)
+        }
     }
 
-    pub fn set_attr(&mut self, channel: SensorChannel, attribute: SensorAttribute, value: SensorValue) -> Result<(), ()> {
-        attr_set(self.device, channel, attribute, value)
+    /// Set the attribute of the channel to the given sensor value.
+    ///
+    /// This method might fail if the sensor does not expose the driver API, the sensor channel or
+    /// the channel does not support the sensor attribute.
+    pub fn set_attr(
+        &mut self,
+        channel: SensorChannel,
+        attribute: SensorAttribute,
+        value: SensorValue,
+    ) -> Result<(), ZephyrError> {
+        // device is required to be a sensor device in constructor
+        unsafe { attr_set(self.device, channel, attribute, value) }
     }
 
-    pub fn set_lower_threshold(&mut self, channel: SensorChannel, value: SensorValue) -> Result<(), ()> {
+    /// Set the [SensorAttribute::LowerThreshold] attribute of `channel`. See [Sensor::set_attr] for details.
+    pub fn set_lower_threshold(
+        &mut self,
+        channel: SensorChannel,
+        value: SensorValue,
+    ) -> Result<(), ZephyrError> {
         self.set_attr(channel, SensorAttribute::LowerThreshold, value)
     }
 
-    pub fn set_upper_threshold(&mut self, channel: SensorChannel, value: SensorValue) -> Result<(), ()> {
+    /// Set the [SensorAttribute::UpperThreshold] attribute of `channel`. See [Sensor::set_attr] for details.
+    pub fn set_upper_threshold(
+        &mut self,
+        channel: SensorChannel,
+        value: SensorValue,
+    ) -> Result<(), ZephyrError> {
         self.set_attr(channel, SensorAttribute::UpperThreshold, value)
     }
 
-    pub fn enable_trigger(&mut self, trigger_type: TriggerType, channel: SensorChannel, f: extern "C" fn(dev: &'static Device, trigger: &SensorTrigger)) -> Result<(), ()> {
+    /// Install a trigger of type `trigger_type` on `channel`.
+    ///
+    /// `f` is the callback function. The callback function will be called with the device the trigger
+    /// has occurred on and the configuration that has been passed with the trigger.
+    ///
+    /// The callback uses a few tricks described in [The Nomicon](https://doc.rust-lang.org/nomicon/ffi.html#the-nullable-pointer-optimization)
+    /// to allow the usage of Rust types in the callback. Also note that the callback [MUST NOT panic](https://doc.rust-lang.org/nomicon/ffi.html#ffi-and-panics).
+    pub fn enable_trigger(
+        &mut self,
+        trigger_type: TriggerType,
+        channel: SensorChannel,
+        f: extern "C" fn(dev: &'static Device, trigger: &SensorTrigger),
+    ) -> Result<(), ZephyrError> {
         let sensor_trigger = SensorTrigger {
             type_: trigger_type as u32,
             chan: channel as u32,
         };
-        trigger_set(self.device, &sensor_trigger, f)
+        /// device is required to be a sensor device in constructor
+        unsafe {
+            trigger_set(self.device, &sensor_trigger, f)
+        }
     }
 }
 
+#[derive(Debug)]
+struct SensorWrapperContext {}
+
+impl Context for SensorWrapperContext {
+    fn name(&self) -> &'static str {
+        "sensor wrapper"
+    }
+}
